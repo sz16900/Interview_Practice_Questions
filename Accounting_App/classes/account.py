3c85f06b592491f907e47c397c71eee0f1107a37
Seth <sz16900@my.bristol.ac.uk>
2017-11-14 21:58:17 -0700

    Account class with test

---
+++ Accounting_App/classes/account.py
@@ -0,0 +1,40 @@
+# import functools import total_ordering
+#
+# @total_ordering
+class Account:
+
+    def __init__(self, owner, amount=0):
+
+        self.owner = owner
+        self.amount = amount
+        self._transactions = []
+
+    def __str__(self):
+        return "Account of %s with starting amount: %s" % (
+            self.owner, self.amount)
+
+    def add_transaction(self, amount):
+        if not isinstance(amount, int):
+            raise ValueError('please use int for amount')
+        else:
+            self._transactions.append(amount)
+
+    @property
+    def balance(self):
+        return self.amount + sum(self._transactions)
+
+
+    def __len__(self):
+        return len(self._transactions)
+
+    def __getitem__(self, position):
+        return self._transactions[position]
+
+    def __reversed__(self):
+        return self[::-1]
+
+    def __eq__(self, other):
+        return self.balance == other.balance
+
+    def __lt__(self, other):
+        return self.balance < other.balance
---
+++ Accounting_App/test.py
@@ -0,0 +1,18 @@
+from classes.account import Account
+import pytest
+
+class TestClass(object):
+    def test_one(self):
+        acc = Account('bob', 10)
+        assert 'Account of bob with starting amount: 10' in str(acc)
+        acc2 = Account('tom', 40)
+        assert 'Account of tom with starting amount: 40' in str(acc2)
+        acc.add_transaction(20)
+        acc.add_transaction(-10)
+        acc.add_transaction(50)
+        acc.add_transaction(-20)
+        acc.add_transaction(30)
+        assert acc.balance == 80
+        assert len(acc) == 5
+        assert acc[1] == -10
+        assert list(reversed(acc)) == [30, -20, 50, -10, 20]
